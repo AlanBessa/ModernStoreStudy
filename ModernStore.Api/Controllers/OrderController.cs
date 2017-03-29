e95a23da0a26f7db5ee736bde7d6f7502cd3176c
Alan Bessa <bessa.alan@gmail.com>
2017-03-28 23:53:46 -0300

    API basico: arquivo esquecido

--- ModernStore.Api/Controllers/OrderController.cs
+++ ModernStore.Api/Controllers/OrderController.cs
@@ -2,10 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ModernStore.Infra.Transactions;
+using ModernStore.Domain.Commands.Handlers;
+using Microsoft.AspNetCore.Mvc;
+using ModernStore.Domain.Commands.Inputs;
 
 namespace ModernStore.Api.Controllers
 {
-    public class OrderController
+    public class OrderController : BaseController
     {
+        private readonly OrderCommandHandler _handler;
+
+        public OrderController(IUow uow, OrderCommandHandler handler) : base(uow)
+        {
+            _handler = handler;
+        }
+
+        [HttpPost]
+        [Route("v1/orders")]
+        public async Task<IActionResult> Post([FromBody]RegisterOrderCommand command)
+        {
+            var result = _handler.Handle(command);
+            return await Response(result, _handler.Notifications);
+        }
     }
 }
